eab08392b37edd969072cf25846172bd2abbb483
slealnovoa <116236674+slealnovoa@users.noreply.github.com>
2023-03-13 18:11:28 -0400

    Add files via upload

---
+++ TwitterApp/Assignment 2 Santiago Leal Novoa 301093542/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Assignment_2_Santiago_Leal_Novoa_301093542.Models;
+
+namespace Assignment_2_Santiago_Leal_Novoa_301093542
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TweetManager.Initialize("0");
+            TweetManager.ShowAll();
+            TweetManager.ShowAll("Raptors");
+        }
+    }
+}
